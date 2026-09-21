df001cbb69cd90902ed5226dcceae5c7a4741418
alok ku dash <dashalok003@gmail.com>
2026-09-21 15:38:00 +0530

    Connected frontend to live Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Our backend URL
+  baseURL: 'https://melodyhub-ai.onrender.com/api', // Our backend URL
 });
 
 // Automatically add the token to requests whether stored as standalone or inside userInfo
